d5db177e478d3004ce8dc65d814636b5d313c8a3
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-23 00:29:47 -0600

    adds logic to login route to conditionalize return

--- app/routes.py
+++ app/routes.py
@@ -20,5 +20,9 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/login')
+
+def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
